b74ab259961d6b1f1f47d6cc63d07424d9ec6710
Work <Work@Kyles-MacBook-Pro.local>
2018-06-12 10:50:55 -0500

    update

--- testPy.py
+++ testPy.py
@@ -1,10 +1,22 @@
-from h2o.grid.grid_search import H2OGridSearch
-from h2o.estimators.deeplearning import H2ODeepLearningEstimator
-
-hidden_opt = [[32,32],[32,16,8],[100]]
-l1_opt = [1e-4,1e-3]
-hyper_parameters = {"hidden":hidden_opt, "l1":l1_opt}
-
-model_grid = H2OGridSearch(H2ODeepLearningEstimator, hyper_params=hyper_parameters)
-model_grid.train(x=x, y=y, distribution="multinomial", epochs=1000, training_frame=train,
-   validation_frame=test, score_interval=2, stopping_rounds=3, stopping_tolerance=0.05, stopping_metric="misclassification")
+import h2o
+from h2o.automl import H2OAutoML
+h2o.init()
+df = h2o.import_file("https://raw.githubusercontent.com/h2oai/sparkling-water/master/examples/smalldata/prostate.csv")
+train, test = df.split_frame(ratios=[.9])
+# Identify predictors and response
+x = train.columns
+y = "CAPSULE"
+x.remove(y)
+# For binary classification, response should be a factor
+train[y] = train[y].asfactor()
+test[y] = test[y].asfactor()
+# Run AutoML for 60 seconds
+aml = H2OAutoML(max_runtime_secs = 60)
+aml.train(x = x, y = y, training_frame = train, leaderboard_frame = test)
+# View the AutoML Leaderboard
+aml.leaderboard
+aml.leader
+# To generate predictions on a test set, use `"H2OAutoML"` object, or on the leader model object directly as below:
+preds = aml.predict(test)
+# or
+preds = aml.leader.predict(test)
