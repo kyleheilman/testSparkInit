b35017d00d346f5219a96fd16902ae09cda79de8
kyleheilman <35972479+kyleheilman@users.noreply.github.com>
2018-06-12 13:02:29 -0500

    Update testPy.py

--- testPy.py
+++ testPy.py
@@ -5,7 +5,7 @@ from pyspark.sql import SparkSession
 # Initiate SparkSession
 spark = SparkSession.builder.appName("App name").getOrCreate()
 spark.addPyFile("/opt/spark/dist/sparkling-water-2.2.16.zip")
-import "sparkling-water-2.2.16.zip" as h2o
+import "/opt/spark/dist/sparkling-water-2.2.16.zip" as h2o
 
 # Initiate H2OContext
 hc = H2OContext.getOrCreate(spark)
