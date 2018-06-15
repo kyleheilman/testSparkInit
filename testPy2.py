6122928bc0d87f561ac130a7eb1bd97648955096
kyleheilman <35972479+kyleheilman@users.noreply.github.com>
2018-06-14 22:30:59 -0500

    Create testPy2.py

---
+++ testPy2.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext, SparkFiles
+import sys
+
+# Initiate SparkSession
+spark = SparkSession\
+    .builder\
+    .appName("PythonPi")\
+    .getOrCreate()
+
+spark.sparkContext.addPyFile("/mnt/mesos/sandbox/sparkling-water-2.2.16.zip")
+
+sys.path.insert(0, '/mnt/mesos/sandbox/sparkling-water-2.2.16/py/build/dist/h2o_pysparkling_2.2-2.2.16.zip')
+print(SparkFiles.getRootDirectory())
+print(help('modules'))
+print('\n'.join(sys.path))
+
+import h2o
+from pysparkling import *
+# Initiate H2OContext
+hc = H2OContext.getOrCreate(spark)
+
+pFile = spark.read.orc("s3://dfs-lab13-ace/testLarge3/orctestdata/")
+
+h2oFrame = h2oContext.asH2OFrame(pFile)
+
+# Stop H2O and Spark services
+h2o.cluster().shutdown()
+spark.stop()
